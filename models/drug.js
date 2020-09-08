fc970d7681f2c3ccd8cfaa490bbe63a1d0b3b3fb
Thibault de Saint Sernin <thibault.de-saint-sernin@gadz.org>
2020-09-08 10:23:40 +0200

    refactor(Drug): add clear setters and getters

--- models/drug.js
+++ models/drug.js
@@ -1,7 +1,27 @@
 export default class Drug {
   constructor(name, expiresIn, benefit) {
-    this.name = name;
-    this.expiresIn = expiresIn;
-    this.benefit = benefit;
+    this._name = name;
+    this._expiresIn = expiresIn;
+    this._benefit = benefit;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get expiresIn() {
+    return this._expiresIn;
+  }
+
+  set expiresIn(value) {
+    this._expiresIn = value;
+  }
+
+  get benefit() {
+    return this._benefit;
+  }
+
+  set benefit(value) {
+    this._benefit = value;
   }
 }
